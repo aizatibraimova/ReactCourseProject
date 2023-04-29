6cada1dea2e8f6f6e77cba08c4646ee7c53f07aa
Aizat I <ibraimova.as@gmail.com>
2023-04-28 23:40:58 -0500

    CampsitesDirectoryPage and CampsiteDetail

--- src/features/campsites/campsitesSlice.js
+++ src/features/campsites/campsitesSlice.js
@@ -4,6 +4,6 @@ export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-export const selectrandomCampsite = () => {
-    return  CAMPSITES;[Math.floor(CAMPSITES.length * Math.random())] ;
+export const selectRandomCampsite = () => {
+    return  CAMPSITES[Math.floor(CAMPSITES.length * Math.random())] ;
 }
