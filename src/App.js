97a737810223569ebee9313ce1a6b57758ca72f3
aizatibraimova <97326728+aizatibraimova@users.noreply.github.com>
2023-05-19 21:01:39 -0500

    Async Promotions

--- src/App.js
+++ src/App.js
@@ -11,13 +11,15 @@ import "./App.css";
 import AboutPage from "./pages/AboutPage";
 import { fetchCampsites } from "./features/campsites/campsitesSlice";
 import { fetchPartners } from "./features/partners/partnersSlice";
+import { fetchPromotions } from "./features/promotions/promotionsSlice";
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(fetchCampsites());
-    dispatch(fetchPartners())
+    dispatch(fetchPartners());
+    dispatch(fetchPromotions())
   }, [dispatch]);
   
   return (
